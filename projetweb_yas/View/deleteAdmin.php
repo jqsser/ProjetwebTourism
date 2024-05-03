987aefeb62315af821cc3034a7ba1bd2db462fed
Jasser Jebali <131884928+jqsser@users.noreply.github.com>
2024-05-03 10:01:19 +0100

    test yassssss

--- projetweb_yas/View/deleteAdmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../Controller/ReservationC.php';
-
-
-$ReservationC = new ReservationC();
-$ReservationC->delete_reservation($_POST["id_reserv"]);
-header('Location:AdminListe.php');
-
-?>
